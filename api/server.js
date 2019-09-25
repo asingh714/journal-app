e2d6b931cac12c02fd99a6f145a61fa56ecfd062
Aman <amsingh714@gmail.com>
2019-09-25 11:24:55 -0400

    added some middleware

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
+const middleware = require("./middleware");
 const express = require("express");
 
 const server = express();
+middleware(server);
 
 server.get("/", (req, res) => {
   res.send(`<h1>MyJournal API</h1>`)
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const server = require("./api/server");
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log(`\n*** Server is running on http://localhost:${port} ***`);
