4dec51390ebed060b6b5dc74df3cfb76d6a4b0a0
Aman <amsingh714@gmail.com>
2019-09-25 12:16:29 -0400

    added some starter files for auth and packages: knex, sqlite3

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const middleware = require("./middleware");
 const express = require("express");
 
+const authRouter = require("../auth/auth-router");
+
 const server = express();
 middleware(server);
+
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h1>MyJournal API</h1>`)
