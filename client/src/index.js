2bd0c7eda10024f373b3ffc6ab8a30dbea408568
Aman <amsingh714@gmail.com>
2019-09-27 16:00:04 -0400

    set up store and basic reducer to get started

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from "./redux/reducers";
+
+import "./index.css";
+import App from "./App";
+
+const middlewares = [];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
