1a770413274b92f0a68421271a0641980f44380c
Aman <amsingh714@gmail.com>
2019-10-06 16:06:45 -0400

    added login and sign up page

--- client/src/pages/log-in-and-sign-up/log-in-and-sign-up.component.jsx
+++ client/src/pages/log-in-and-sign-up/log-in-and-sign-up.component.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+
+import "./log-in-and-sign-up.component.styles.scss";
 
 const LogInAndSignUp = () => {
   return (
-    <div>
-      log in and sign up 
-    </div>
-  )
-}
+    <div className="page-container">
+      <div className="log-in-sign-in-container">
+        <form className="log-in-container">
+          <h2>Login</h2>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button>Login</button>
+        </form>
 
-export default LogInAndSignUp
+        <form className="sign-in-container">
+          <h2>Sign Up</h2>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Confirm Password" />
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LogInAndSignUp;
