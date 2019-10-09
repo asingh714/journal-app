87626b87e4f3a7ae25337bca5fd1f07816fa203f
Aman <amsingh714@gmail.com>
2019-10-09 13:47:54 -0400

    added hero section content

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import CustomButton from "../../components/custom-button/custom-button.component";
+
+import "./homepage.styles.scss";
 
 const Homepage = () => {
   return (
-    <div>
-      Homepage
+    <div className="hero-section">
+      <h1>
+        Your morning and evening journal. <br /> Secure, free and online.
+      </h1>
+      <h2>A science back method to increase <br /> happiness and reduce stress.</h2>
+      <CustomButton>Sign Up</CustomButton>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
