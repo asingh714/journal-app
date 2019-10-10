8c8b7ded8de6dfd514d6de2099da8aa3ce0e3034
Aman <amsingh714@gmail.com>
2019-10-10 18:55:12 -0400

    styles for login and sign in page complete

--- client/src/pages/log-in-and-sign-up/log-in-and-sign-up.component.jsx
+++ client/src/pages/log-in-and-sign-up/log-in-and-sign-up.component.jsx
@@ -7,6 +7,8 @@ import useForm from "../../customHooks/useForm";
 import CustomButton from "../../components/custom-button/custom-button.component";
 import FormInput from "../../components/form-input/form-input.component";
 
+import UserImg from "../../assets/User-Image.png";
+
 import "./log-in-and-sign-up.component.styles.scss";
 
 const LogInAndSignUp = ({ loginUser, registerUser, history }) => {
@@ -18,7 +20,7 @@ const LogInAndSignUp = ({ loginUser, registerUser, history }) => {
 
   const handleSignin = event => {
     if (event) event.preventDefault();
-    registerUser(signinInput)
+    registerUser(signinInput);
     history.push("/journal");
   };
 
@@ -31,60 +33,79 @@ const LogInAndSignUp = ({ loginUser, registerUser, history }) => {
 
   return (
     <div className="page-container">
-      <div className="log-in-sign-in-container">
-        <form className="log-in-container">
-          <h2 className="header">Login</h2>
-          <FormInput
-            name="username"
-            placeholder="Username"
-            handleChange={handleLoginChanges}
-            type="text"
-            value={loginInput.username || ""}
-            isShort
-          />
-          <FormInput
-            name="password"
-            placeholder="Password"
-            handleChange={handleLoginChanges}
-            type="password"
-            value={loginInput.password || ""}
-            isShort
-          />
-        <CustomButton isWideButton onClick={handleLoginSubmit}>Login</CustomButton>
-        </form>
+      <div className="log-in-sign-in-quote-container">
+        <div className="form-container">
+          <form className="log-in-container">
+            <h2 className="header">Login</h2>
+            <FormInput
+              name="username"
+              placeholder="Username"
+              handleChange={handleLoginChanges}
+              type="text"
+              value={loginInput.username || ""}
+              isShort
+            />
+            <FormInput
+              name="password"
+              placeholder="Password"
+              handleChange={handleLoginChanges}
+              type="password"
+              value={loginInput.password || ""}
+              isShort
+            />
+            <CustomButton isWideButton onClick={handleLoginSubmit}>
+              Login
+            </CustomButton>
+          </form>
 
-        <form className="sign-in-container" >
-          <h2 className="header">Sign Up</h2>
-          <FormInput
-            name="name"
-            placeholder="Name"
-            handleChange={handleSigninChanges}
-            type="text"
-            value={signinInput.name || ""}
-          />
-          <FormInput
-            name="email"
-            placeholder="Email"
-            handleChange={handleSigninChanges}
-            type="email"
-            value={signinInput.email || ""}
-          />
-          <FormInput
-            name="username"
-            placeholder="Username"
-            handleChange={handleSigninChanges}
-            type="text"
-            value={signinInput.username || ""}
-          />
-          <FormInput
-            name="password"
-            placeholder="Password"
-            handleChange={handleSigninChanges}
-            type="password"
-            value={signinInput.password || ""}
-          />
-          <CustomButton isWideButton onClick={handleSigninSubmit}>Submit</CustomButton>
-        </form>
+          <form className="sign-in-container">
+            <h2 className="header">Sign Up</h2>
+            <FormInput
+              name="name"
+              placeholder="Name"
+              handleChange={handleSigninChanges}
+              type="text"
+              value={signinInput.name || ""}
+            />
+            <FormInput
+              name="email"
+              placeholder="Email"
+              handleChange={handleSigninChanges}
+              type="email"
+              value={signinInput.email || ""}
+            />
+            <FormInput
+              name="username"
+              placeholder="Username"
+              handleChange={handleSigninChanges}
+              type="text"
+              value={signinInput.username || ""}
+            />
+            <FormInput
+              name="password"
+              placeholder="Password"
+              handleChange={handleSigninChanges}
+              type="password"
+              value={signinInput.password || ""}
+            />
+            <CustomButton isWideButton onClick={handleSigninSubmit}>
+              Submit
+            </CustomButton>
+          </form>
+        </div>
+
+        <div className="line"></div>
+
+        <div className="testimonial-quote">
+          <span className="quote">
+            “The MyJournal app has made me at least 25% happier”
+            <br />
+            <div className="user-img-container">
+              <span className="user">— Luke Williams</span>
+              <img src={UserImg} alt="Happy User" />
+            </div>
+          </span>
+        </div>
       </div>
     </div>
   );
