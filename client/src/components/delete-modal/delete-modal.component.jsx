f63d62be6523d5d8f1b55f774e2749149d93490d
Aman <amsingh714@gmail.com>
2019-10-14 15:04:40 -0400

    styled the delete modal

--- client/src/components/delete-modal/delete-modal.component.jsx
+++ client/src/components/delete-modal/delete-modal.component.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { deleteJournalEntry } from "../../redux/actions/entries.actions";
+import CustomButton from "../custom-button/custom-button.component";
+
+import "./delete-modal.styles.scss";
 
 const DeleteModal = ({ toggleDelete, deleteJournalEntry, deleteId }) => {
   const handleDelete = () => {
@@ -9,11 +12,19 @@ const DeleteModal = ({ toggleDelete, deleteJournalEntry, deleteId }) => {
   };
 
   return (
-    <>
+    <div className="delete-modal">
+
       <span>Are you sure you want to delete this journal entry?</span>
-      <button onClick={toggleDelete}>No</button>
-      <button onClick={handleDelete}>Yes</button>
-    </>
+      
+      <div className="button-container">
+        <CustomButton isDelete onClick={toggleDelete}>
+          No
+        </CustomButton>
+        <CustomButton isBigEdit onClick={handleDelete}>
+          Yes
+        </CustomButton>
+      </div>
+    </div>
   );
 };
 
