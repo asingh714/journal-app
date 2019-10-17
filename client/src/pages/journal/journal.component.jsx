69478e04653e7e5b1637ce84f8900bceabe17027
Aman <amsingh714@gmail.com>
2019-10-17 16:14:20 -0400

    loading spinner works

--- client/src/pages/journal/journal.component.jsx
+++ client/src/pages/journal/journal.component.jsx
@@ -5,17 +5,17 @@ import "./journal.styles.scss";
 
 import Entry from "../../components/entry/entry.component";
 import CustomButton from "../../components/custom-button/custom-button.component";
-
+import Loader from "react-loader-spinner";
 import { fetchEntries } from "../../redux/actions/entries.actions";
 
-const Journal = ({ fetchEntries, entries, ...props }) => {
+const Journal = ({ fetchEntries, entries, isFetching, ...props }) => {
   useEffect(() => {
     fetchEntries();
   }, [fetchEntries]);
 
-  if (entries.length < 0) {
-    return <h2>No entries</h2>;
-  }
+  // if (entries.length < 1) {
+  //   return <h2>No entries</h2>;
+  // }
 
   const routeToAddJournalEntry = e => {
     e.preventDefault();
@@ -28,6 +28,13 @@ const Journal = ({ fetchEntries, entries, ...props }) => {
         <CustomButton isRounded onClick={routeToAddJournalEntry}>
           Add Journal Entry
         </CustomButton>
+        {/* {entries.length < 1 && <h2>There are no entries</h2>} */}
+        {isFetching && (
+          <div className="key spinner">
+            <Loader type="Puff" color="#f7d070" height="60" width="60" />
+            <p>Loading Data</p>
+          </div>
+        )}
         <div className="journal-entries-container">
           {entries.map(entry => (
             <Entry key={entry.id} entry={entry} {...props} />
@@ -40,7 +47,8 @@ const Journal = ({ fetchEntries, entries, ...props }) => {
 
 const mapStateToProps = state => {
   return {
-    entries: state.entries.entries
+    entries: state.entries.entries,
+    isFetching: state.entries.isFetching
   };
 };
 
